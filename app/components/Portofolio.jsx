2b5492bd2a8f3186a00f01f0dc8c4f8c7ec11f00
skinet-web <skinet2009@gmail.com>
2023-03-15 00:52:14 +0200

    Added work section for mobile

---
+++ app/components/Portofolio.jsx
@@ -0,0 +1,73 @@
+'use client';
+
+import {React, useState, useEffect} from 'react'
+import {AiFillEye, AiFillGithub} from 'react-icons/ai'
+import { motion } from 'framer-motion';
+import { urlFor, client } from '../client'
+import Image from 'next/image';
+
+const Portofolio = () => {
+    const [activeFilter, setActiveFilter] = useState('All');
+    const [works, setWorks] = useState([]);
+    const [filterWork, setFilterWork] = useState([]);
+
+    useEffect(() => {
+        const query = '*[_type == "works"]';
+
+        client.fetch(query)
+            .then((data) => {
+                setWorks(data);
+                setFilterWork(data);
+          });
+    }, []);
+
+  return (
+    <div className='flex flex-col justify-center font-poppins items-center'>
+          <h1 className='mt-10 text-center font-bold uppercase text-2xl'>Portofolio</h1>
+          <p>Each project is a unique piece of development 🧩</p>  
+
+          <div>
+            {['React JS', 'Tailwind CSS', 'All'].map((item, index) => (
+                <div 
+                    key={index}
+                    onClick={() => handleWorkFilter(item)}
+                    className=''>
+                        {item}
+                </div>
+            ))}
+          </div>
+          <motion.div className='flex flex-col  justify-center items-center '>
+            {filterWork.map((work, index) => (
+                <div key={index} className='flex flex-wrap justify-center items-center
+                 rounded-xl w-[80%] box--shadow mt-10'>
+                    <div className='flex justify-center m-5'>
+                    <Image src={urlFor(work.imgUrl).url()} width='500' height='500' alt='work-name' 
+                        className='box--shadow rounded-xl object-contain'/>
+                    </div>
+                    <div className='flex flex-col   justify-center items-center'>
+                        <h3 className='m-3 font-bold text-xl uppercase'>{work.title}</h3>
+                        <p className='w-[70%]'>{work.description}</p>
+                        <div>
+                            <p className='mt-3'>{work.tags}</p>                            
+                        </div>
+                        <div className='flex m-3 gap-10 justify-center items-center'>
+                            <a className='flex justify-center items-center hover:text-blue-600 
+                            transition-colors duration-500 ease-in-out' 
+                            href={work.codeLink}> 
+                            Code <AiFillGithub className='ml-1 text-2xl'/> 
+                            </a>       
+                            <a className='flex justify-center items-center hover:text-blue-600 
+                            transition-colors duration-500 ease-in-out' 
+                            href={work.projectLink}> 
+                            Live Demo <AiFillEye className='ml-1 text-2xl'/> 
+                            </a>                               
+                        </div>
+                    </div>
+                </div>
+            ))}
+          </motion.div>
+    </div>
+  )
+}
+
+export default Portofolio
--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,7 @@
 import About from "./components/About"
 import Header from "./components/Header"
-import Navbar from "./components/Navbar"
+
+import Portofolio from "./components/Portofolio"
 
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
        
        <Header />
        <About />
+       <Portofolio />
     </div>
   )
 }
