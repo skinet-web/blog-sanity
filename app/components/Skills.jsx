b94ffc50d7d80e06c91b6fe8834615f61d2c6438
skinet-web <skinet2009@gmail.com>
2023-03-18 21:38:01 +0200

    Added skills section

---
+++ app/components/Skills.jsx
@@ -0,0 +1,54 @@
+'use client';
+
+import {React, useState, useEffect} from 'react'
+import { motion } from 'framer-motion';
+import { urlFor, client } from '../client'
+import Image from 'next/image';
+
+const Skills = () => {
+
+    const [skills, setSkills] = useState([])
+
+    const scaleVariants = {
+        whileInView:{
+          scale: [0, 1],
+          opacity: [0, 1],
+          transition: {
+            duration: 0.8,
+            ease: 'easeInOut'
+          }
+        }
+      }
+
+    useEffect(() => {
+        const query = '*[_type == "skills"]';
+
+        client.fetch(query)
+            .then((data) => {
+                setSkills(data);
+          });
+    }, []);
+
+  return (
+    <div className='flex  flex-col justify-center items-center font-poppins'>
+        <h1 className='mt-10  font-bold'>
+        Tech Stack 
+        </h1>
+        <motion.div 
+        variants={scaleVariants}
+        whileInView={scaleVariants.whileInView}
+       
+        className='flex  flex-wrap my-5 gap-5 justify-center items-center'>
+            {skills.map((item, index) => (
+                <div key={index} className='flex bg-[#EDF2F8]  xs:w-[10%] w-[15%] rounded-xl justify-center items-center '>
+                    <Image src={urlFor(item.imgUrl).url()} width='100' height='100' alt={item.title} title={item.title}
+                    className='object-contain p-2 cursor-pointer'/> 
+                </div>
+                
+            ))}
+        </motion.div>
+    </div>
+  )
+}
+
+export default Skills
--- app/page.jsx
+++ app/page.jsx
@@ -2,14 +2,15 @@ import About from "./components/About"
 import Header from "./components/Header"
 
 import Portofolio from "./components/Portofolio"
+import Skills from "./components/Skills"
 
 
 export default function Home() {
   return (
-    <div className=' '>
-       
+    <div className=' '>       
        <Header />
        <About />
+       <Skills />
        <Portofolio />
     </div>
   )
