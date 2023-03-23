fc549e43390c0819930b96e19fc1c87f2a3bcbe7
skinet-web <skinet2009@gmail.com>
2023-03-23 23:53:01 +0200

    Managed to get dynamic routing working

--- app/blog/[slug]/page.jsx
+++ app/blog/[slug]/page.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { client } from '../../client';
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
+async function getPost(slug) {
+const query = `*[_type == "post" && slug.current == $slug][0]`;
+const params = { slug };
+const post = await client.fetch(query, params);
+return post;
 }
 
-export default page
+export default async function Post({ params }) {
+  
+  const post = await getPost(params.slug);
+
+
+  return (
+    <div>
+      <h1 className='text-[100px]'>{post.title}</h1>
+      <h1 className='text-[100px]'>{post.body}</h1>
+    </div>
+  );
+}
+
--- app/components/Blog.jsx
+++ app/components/Blog.jsx
@@ -1,13 +1,9 @@
 'use client';
 
-
 import {React, useState, useEffect} from 'react'
 import { motion } from 'framer-motion';
 import { urlFor, client } from '../client'
 import Link from 'next/link';
-
-
-
 
 const Blog = () => {
 
@@ -22,13 +18,15 @@ const Blog = () => {
           });
     }, []);
 
+    
+
   return (
     <div id='blog' className='h-[200px]'>
         {post.map((item) => (
             <Link key={item.slug.current} href={`/blog/${item.slug.current}`}>
                 
                 {item.title}
-                {item.description} 
+                {item.body} 
                 
                
             </Link>
