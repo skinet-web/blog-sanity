6d12bf4ad64de5bcfc414c16c800aaf2e69b7744
skinet-web <skinet2009@gmail.com>
2023-03-25 23:28:11 +0200

    Added Blog styles

--- app/components/Blog.jsx
+++ app/components/Blog.jsx
@@ -4,6 +4,7 @@ import {React, useState, useEffect} from 'react'
 import { motion } from 'framer-motion';
 import { urlFor, client } from '../client'
 import Link from 'next/link';
+import Image from 'next/image';
 
 const Blog = () => {
 
@@ -21,13 +22,22 @@ const Blog = () => {
     
 
   return (
-    <div id='blog' className='h-[200px]'>
+    <div id='blog' className='flex flex-col justify-center items-center font-poppins'>
+        <h2 className='mt-20 mb-10 text-center font-bold uppercase text-2xl'>BLOG</h2>
         {post.map((item) => (
-            <Link key={item.slug.current} href={`/blog/${item.slug.current}`}>
+            <Link key={item.slug.current} href={`/blog/${item.slug.current}` } className='flex flex-col justify-center items-center
+            rounded-xl w-[90%] sm:w-[60%] transition-all box--shadow mt-10   bg-white'>
                 
-                {item.title}
-                {item.body} 
-                
+                        <div className='flex justify-center m-5'>
+                        <Image src={urlFor(item.coverImage).url()} width='500' height='500' alt='work-name' 
+                            className='box--shadow rounded-xl  object-contain'/>
+                        </div>
+                        <div className='flex flex-col  justify-center items-center'>
+                            <h3 className='m-3 font-bold text-xl uppercase'>{item.title}</h3>
+                            <p className='w-[80%] text-justify text-[#767676] '>{item.body}</p>
+                                                    
+                        </div>
+                                          
                
             </Link>
         ))}
