b7e7ea43e7343f23d80531ac2d7504e19f0ff496
skinet-web <skinet2009@gmail.com>
2023-03-27 23:00:33 +0300

    Bug with socialmedia styles

--- app/components/SocialMedia.jsx
+++ app/components/SocialMedia.jsx
@@ -8,7 +8,8 @@ const SocialMedia = () => {
         {/* <div className={`${styles.socialIcon}`}>
             <BsGlobe />
         </div > */}
-        <a href='https://github.com/skinet-web'className={ styles.socialIcon}>
+        <a href='https://github.com/skinet-web'className={`w-10 h-10 rounded-full  bg-white my-1 border border-gray-300 
+        ${styles.socialIcon}`}>
            <BsGithub />
         </a>
         <a href='https://www.linkedin.com/in/mihai-s%C4%83cuiu-2a1b29167/' className={styles.socialIcon}>
